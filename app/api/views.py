4d9a7167553aba366a450aeb29c3edef64df3d8d
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-23 12:04:29 -0500

    init

---
+++ app/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+@api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+def firstFunction(request):
+    # if request.method == "POST":
+    print(request.query_params)
+
+    return Response({'message': request.query_params})
