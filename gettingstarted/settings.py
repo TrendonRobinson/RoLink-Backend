6c5347e0f6bccfb8b231d0889cd79fca3af0230e
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-23 11:58:29 -0500

    init

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -33,13 +33,26 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "app",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # 3rd Party
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    # 'app_label',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
+    # Apps
+    'app'
 ]
 
 MIDDLEWARE = [
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,21 +1,11 @@
-# import hello.views
+from django import urls
+from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib import admin
-
-admin.autodiscover()
-
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
-    path("admin/", admin.site.urls)  # ,
-    # path("", hello.views.index, name="index"),
-    # path("db/", hello.views.db, name="db"),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("app/", include('app.api.urls'))
 ]
