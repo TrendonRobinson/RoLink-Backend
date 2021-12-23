1323d1c61060c22d099d3cb63bd550451868b7d5
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-23 12:23:57 -0500

    fix

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserPost(models.Model):
+    post_content = models.CharField(max_length=200)
+
+
+class Game(models.Model):
+    test_text = models.CharField(max_length=200)
